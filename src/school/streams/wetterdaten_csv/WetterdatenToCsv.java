381b5d15bd607a38f727d6b4936194b5eff25b5c
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-10-07 20:30:57 +0200

    feat: format the output according to the task

---
+++ src/school/streams/wetterdaten_csv/WetterdatenToCsv.java
@@ -0,0 +1,44 @@
+package school.streams.wetterdaten_csv;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class WetterdatenToCsv {
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader in = new BufferedReader(new FileReader("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.txt"));
+             PrintWriter out = new PrintWriter(new FileWriter("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.csv"));) {
+            String line = null;
+            String temp = null;
+
+            String[] splitted = null;
+
+            while ((line = in.readLine()) != null) {
+                if (line.charAt(0) == '#') {
+                    continue;
+                }
+
+                if (line.contains("_")) {
+                    temp = line;
+                    continue;
+                }
+
+                splitted = line.split(" +");
+
+                for (int i = 0; i < splitted.length; i++) {
+                    if (splitted[0].charAt(0) == 'D') {
+                        splitted[0] = temp;
+                    } else {
+                        splitted[0] = splitted[0].substring(0, 2);
+                    }
+
+                    if (i == splitted.length - 1) {
+                        out.write(splitted[i] + "\n");
+                    } else {
+                        out.write(splitted[i] + ";");
+                    }
+                }
+            }
+        }
+    }
+}
