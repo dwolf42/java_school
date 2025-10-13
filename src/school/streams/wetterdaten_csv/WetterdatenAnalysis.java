88e44d8e85ed39ca22e99768b1d1afc6a2fad1e0
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-10-13 22:36:25 +0200

    add wip: traverse data document and parse values to double

--- src/school/streams/wetterdaten_csv/WetterdatenAnalysis.java
+++ src/school/streams/wetterdaten_csv/WetterdatenAnalysis.java
@@ -2,6 +2,7 @@ package school.streams.wetterdaten_csv;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WetterdatenAnalysis {
     public static void main(String[] args) throws IOException {
@@ -19,69 +20,37 @@ public class WetterdatenAnalysis {
                 out.write(line + "\n");
             }
 
-            // Initialized where it's acutally needed
+            // Initialized where it's actually needed
             String[] splitted = null;
-            ArrayList<ArrayList<Double>> wValues = new ArrayList<ArrayList<Double>>();
+            double[][] wValues = new double[34][8];
 
+            int wValuesRow = 0;
             while ((line = in.readLine()) != null) {
                 splitted = line.split(" +");
-                ArrayList<Double> row = new ArrayList<>();
 
                 // Iterate over the read line which has been split to an array.
                 // If the index is parseable to Double, it gets written to the row.
-                for (int i = 0; i < splitted.length; i++) {
+                for (int splittedIterator = 1; splittedIterator < splitted.length; splittedIterator++) {
                     try {
-                        row.add(Double.parseDouble(splitted[i]));
+                        wValues[wValuesRow][splittedIterator] = Double.parseDouble(splitted[splittedIterator]);
                     } catch (NumberFormatException e) {
                         // Nothing here
                     }
-
                 }
+                wValuesRow++;
 
-                // At the end of each iterated line, the row it put into the 2d ArrayList
-                wValues.add(row);
             }
 
+            for (int i = 0; i < wValues.length; i++) {
+                for (int j = 0; j < wValues[i].length; j++) {
+                    System.out.print(wValues[i][j] + " ");
+                }
+                System.out.println("");
+            }
             // Calculation
-            ArrayList<Double> minimum = new ArrayList<Double>();
-            ArrayList<Double> mittel = new ArrayList<Double>();
-            ArrayList<Double> maxmimum = new ArrayList<Double>();
 
             Double temp;
 
-//            for (int i = 0; i < wValues.get())
-
-                /*
-                wValues.size 0000000
-                wValues.size 0000000
-                wValues.size 0000000
-                 */
-//            for (int i = 0; i < wValues.size(); i++) {
-//                for (int j = 0; j < wValues.get(i).size(); j++) {
-//                    out.write(wValues.get(i).get(j) + " ");
-//                }
-//                out.write("\n");
-//            }
-
-// Find the maximum number of columns
-            int maxCols = 0;
-            for (ArrayList<Double> row : wValues) {
-                if (row.size() > maxCols) {
-                    maxCols = row.size();
-                }
-            }
-
-// Iterate vertically (column by column)
-            for (int j = 0; j < maxCols; j++) {
-                for (int i = 0; i < wValues.size(); i++) {
-                    if (j < wValues.get(i).size()) {
-                        out.write(wValues.get(i).get(j) + " ");
-                    } else {
-                        out.write("  "); // or skip, or write a placeholder
-                    }
-                }
-                out.write("\n");
-            }
 
 
         }
