5074a305287ac83dccdc9ee093eeee981c5bc864
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-10-14 17:42:23 +0200

    add: the approach of my teacher to solve this problem

--- src/school/streams/wetterdaten_csv/WetterdatenToCsv.java
+++ src/school/streams/wetterdaten_csv/WetterdatenToCsv.java
@@ -1,46 +1,80 @@
 package school.streams.wetterdaten_csv;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class WetterdatenToCsv {
     public static void main(String[] args) throws IOException {
-//        String csvfilename = filename.substring(0, filename.lastindexOf('.')) + ".csv";
-        try (BufferedReader in = new BufferedReader(new FileReader("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.txt"));
-             PrintWriter out = new PrintWriter(new FileWriter("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.csv"));) {
+       teachersApproach();
+    }
+
+    public static void myApproach() throws IOException {
+        try (BufferedReader in = new BufferedReader(
+                new FileReader("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.txt"));
+             PrintWriter out = new PrintWriter(
+                     new FileWriter("src/school/streams/wetterdaten_csv/wetterdaten_feldberg.csv"));) {
             String line = null;
             String temp = null;
-
             String[] splitted = null;
-
             while ((line = in.readLine()) != null) {
                 if (line.charAt(0) == '#') {
                     continue;
                 }
-
                 if (line.contains("_")) {
                     temp = line;
                     continue;
                 }
-// out.print or out.println lets you write into the file sequentially
-
                 splitted = line.split(" +");
-
                 for (int i = 0; i < splitted.length; i++) {
                     if (splitted[0].charAt(0) == 'D') {
                         splitted[0] = temp;
                     } else {
                         splitted[0] = splitted[0].substring(0, 2);
                     }
-
                     if (i == splitted.length - 1) {
                         out.write(splitted[i] + "\n");
                     } else {
-                        splitted[i] = splitted[i].replaceAll("\\.",",");
+                        splitted[i] = splitted[i].replaceAll("\\.", ",");
                         out.write(splitted[i] + ";");
                     }
                 }
             }
+        }
+    }
+
+    public static void teachersApproach() throws IOException {
+        String filename = "src/school/streams/wetterdaten_csv/wetterdaten_feldberg.txt";
+        String csvfilename = "src/school/streams/wetterdaten_csv/wetterdaten_feldberg.csv";
+        char delimiter = ';';
+        try (BufferedReader in = new BufferedReader(new FileReader(filename));
+             PrintWriter out = new PrintWriter(csvfilename);) {
+            String line;
+            String monat = in.readLine();
+            while ((line = in.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    // Kommentare ignorieren
+                } else if (line.startsWith("Datum")) {
+                    // Kopfzeile
+                    out.print(monat);
+                    String[] word = line.split(" +");
+                    for (int i = 1; i < word.length; i++) {
+                        out.print(delimiter);
+                        out.print(word[i]);
+                    }
+                    out.println();
+                } else {
+                    // Datenzeilen
+                    String[] word = line.split(" +");
+                    out.print(word[0].substring(0, 2));
+                    for (int i = 1; i < word.length; i++) {
+                        out.print(delimiter);
+                        out.print(word[i].replace('.', ','));
+                    }
+                    out.println();
+                }
+            }
+            System.out.println("Konvertierung nach " + csvfilename + " erfolgreich");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
