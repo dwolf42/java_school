0c5f823c6b5c1d163bb534bbe38a85e1461cbe07
dwolf <46925680+dwolf42@users.noreply.github.com>
2025-12-11 18:34:19 +0100

    feat wip: add instantiation for runnable implementation

--- src/school/threads/punktausgabe/Main.java
+++ src/school/threads/punktausgabe/Main.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 class Main {
 	public static void main(String[] args) {
 		punktausgabeExtendsThread();
+		punktausgabeImplementsRunnable();
+	}
+
+	public static void punktausgabeImplementsRunnable() {
+		PunktausgabeRunnable r1 = new PunktausgabeRunnable("^", 1000);
+		Thread t1 = new Thread(r1);
+		t1.start();
+
+		Scanner scanner = new Scanner(System.in);
+		String eingabe;
+
+		while (true) {
+			eingabe = scanner.nextLine();
+			System.out.println(eingabe);
+		}
 	}
 
 	public static void punktausgabeExtendsThread() {
-		Punktausgabe t1 = new Punktausgabe(".", 1000);
+		PunktausgabeThread t1 = new PunktausgabeThread(".", 1000);
 		t1.start();
 
 		Scanner scanner = new Scanner(System.in);
--- src/school/threads/punktausgabe/PunktausgabeThread.java
+++ src/school/threads/punktausgabe/PunktausgabeThread.java
@@ -1,10 +1,10 @@
 package school.threads.punktausgabe;
 
-class Punktausgabe extends Thread {
+class PunktausgabeThread extends Thread {
 	private String text;
 	private int warteZeit;
 
-	public Punktausgabe(String text, int wZeit) {
+	public PunktausgabeThread(String text, int wZeit) {
 		this.text = text;
 		this.warteZeit = wZeit;
 	}
