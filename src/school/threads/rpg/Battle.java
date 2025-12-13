1b0337cc1d3aa41d9ff9857fba8229faa5eb09a3
David Wolf <46925680+dwolf42@users.noreply.github.com>
2025-12-14 00:05:22 +0100

    wip: add conditions to end program

--- src/school/threads/rpg/Battle.java
+++ src/school/threads/rpg/Battle.java
@@ -16,21 +16,25 @@ class Battle {
 	}
 
 	public void start() {
-		StatusPrinter statusPrinter = new StatusPrinter();
-		Thread t1 = new Thread(statusPrinter);
+		StatusPrinter statusThread = new StatusPrinter(2000);
+		Thread t1 = new Thread(statusThread);
 		t1.start();
 
 		Scanner scanner = new Scanner(System.in);
 		String input;
-
-		while (true) {
+		boolean hasStopped = false;
+		while (!hasStopped) {
 			input = scanner.nextLine();
 			if (input.equalsIgnoreCase("a")) {
 				// Hero attacks monster (reduce monster's HP)
-			} else if (input.equalsIgnoreCase("q")) {
+			} else if (input.equalsIgnoreCase("q") ||
+					heroHp <= 0 ||
+					monsterHp <= 0) {
 				// Terminates Thread gracefully => in Thread there must run a !isInterrupted
+
+				hasStopped = true;
 			} else {
-				// error message
+				System.err.println("Quack!");
 			}
 		}
 	}
@@ -44,8 +48,8 @@ class Battle {
 
 		public void run() {
 			if (!Thread.currentThread().isInterrupted() &&
-				Battle.this.heroHp > 0 &&
-				Battle.this.monsterHp > 0) {
+					Battle.this.heroHp > 0 &&
+					Battle.this.monsterHp > 0) {
 				try {
 					Thread.sleep(intervalMillis);
 					System.out.print(Battle.this.heroName + ": " + Battle.this.heroHp);
@@ -56,10 +60,12 @@ class Battle {
 			} else {
 				if (Battle.this.heroHp <= 0) {
 					System.out.println("~Game over~");
-					System.out.println(Battle.this.heroName + " lost the battle against " Battle.this.monsterName + "...");
+					System.out.println(Battle.this.heroName + " lost the battle against " +
+							Battle.this.monsterName + "...");
 					System.out.println(Battle.this.monsterName + " still has " + Battle.this.monsterHp + "HP left!");
 				} else {
-					System.out.println(Battle.this.heroName + " wins against the evil " + Battle.this.monsterName + "!!!");
+					System.out.println(Battle.this.heroName + " wins against the evil " +
+							Battle.this.monsterName + "!!!");
 					System.out.println("You still have " + Battle.this.heroHp + "HP left!!");
 				}
 			}
---
+++ src/school/threads/rpg/Main.java
@@ -0,0 +1,8 @@
+package school.threads.rpg;
+
+class Main {
+   public static void main(String[] args) {
+       Battle firstBattle = new Battle("Frosty Mage", 50, "Munak", 35);
+       firstBattle.start();
+   }
+}
