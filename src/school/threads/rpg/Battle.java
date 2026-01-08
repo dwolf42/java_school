0509f200db2ca6a280b37ebf47be928577194ba2
David Wolf <46925680+dwolf42@users.noreply.github.com>
2026-01-08 21:12:27 +0100

    feat: add loop to thread

--- src/school/threads/rpg/Battle.java
+++ src/school/threads/rpg/Battle.java
@@ -15,7 +15,7 @@ class Battle {
 		this.monsterHp = monsterHp;
 	}
 
-	public void fight() {
+	public void startMe() {
 		StatusPrinter statusThread = new StatusPrinter(1500);
 		Thread t1 = new Thread(statusThread);
 		t1.start();
@@ -47,26 +47,28 @@ class Battle {
 		}
 
 		public void run() {
-			if (!Thread.currentThread().isInterrupted() &&
-					Battle.this.heroHp > 0 &&
-					Battle.this.monsterHp > 0) {
-				try {
-					Thread.sleep(intervalMillis);
-					System.out.print(Battle.this.heroName + ": " + Battle.this.heroHp + " vs. ");
-					System.err.print(Battle.this.monsterName + ": " + Battle.this.monsterHp + "\n");
-				} catch (InterruptedException e) {
-					System.out.println("Battle was interrupted by some weird Kreuz-Otters");
-				}
-			} else {
-				if (Battle.this.heroHp <= 0) {
-					System.out.println("~Game over~");
-					System.out.println(Battle.this.heroName + " lost the battle against " +
-							Battle.this.monsterName + "...");
-					System.out.println(Battle.this.monsterName + " still has " + Battle.this.monsterHp + "HP left!");
+			while (true) {
+				if (!Thread.currentThread().isInterrupted() &&
+						Battle.this.heroHp > 0 && Battle.this.monsterHp > 0) {
+					try {
+						Thread.sleep(intervalMillis);
+						System.out.print(Battle.this.heroName + ": " + Battle.this.heroHp);
+						System.out.print(" vs. ");
+						System.err.print(Battle.this.monsterName + ": " + Battle.this.monsterHp + "\n");
+					} catch (InterruptedException e) {
+						System.out.println("Battle was interrupted by some weird Kreuz-Otters");
+					}
 				} else {
-					System.out.println(Battle.this.heroName + " wins against the evil " +
-							Battle.this.monsterName + "!!!");
-					System.out.println("You still have " + Battle.this.heroHp + "HP left!!");
+					if (Battle.this.heroHp <= 0) {
+						System.out.println("~Game over~");
+						System.out.println(Battle.this.heroName + " lost the battle against " +
+								Battle.this.monsterName + "...");
+						System.out.println(Battle.this.monsterName + " still has " + Battle.this.monsterHp + "HP left!");
+					} else {
+						System.out.println(Battle.this.heroName + " wins against the evil " +
+								Battle.this.monsterName + "!!!");
+						System.out.println("You still have " + Battle.this.heroHp + "HP left!!");
+					}
 				}
 			}
 		}
